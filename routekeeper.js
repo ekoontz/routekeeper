917fb6ec34897cbff9c11a3034c82cba4fedac02
Eugene Koontz <eugene_koontz@trendmicro.com>
2010-08-09 23:58:27 -0700

    continuing with tutorial: http://code.google.com/apis/maps/documentation/javascript/events.html

--- routekeeper.js
+++ routekeeper.js
@@ -1,10 +1,33 @@
+var map;
+
 function initialize() {
-    var latlng = new google.maps.LatLng(37.46,-122.25);
+    var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
     var myOptions = {
-	zoom: 10,
-	center: latlng,
+	zoom: 4,
+	center: myLatlng,
 	mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    var map = new google.maps.Map(document.getElementById("map_canvas"),
-	myOptions);
+    map = new google.maps.Map(document.getElementById("map_canvas"),
+			      myOptions);
+    
+    google.maps.event.addListener(map, 'zoom_changed', function() {
+	setTimeout(moveToDarwin, 1000);
+    });
+    
+    var marker = new google.maps.Marker({
+	position: myLatlng, 
+	map: map,
+	title:"Hello World!"
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+	map.setZoom(8);
+    });
 }
+  
+  function moveToDarwin() {
+      var darwin = new google.maps.LatLng(-12.461334, 130.841904);
+      map.setCenter(darwin);
+  }
+
+
