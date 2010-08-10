90449439af04a8f83b3870f0b3c5679b58e2bd26
Eugene Koontz <eugene_koontz@trendmicro.com>
2010-08-10 00:10:49 -0700

    adding markers all around Pacific Heights

--- routekeeper.js
+++ routekeeper.js
@@ -1,33 +1,24 @@
 var map;
-
 function initialize() {
-    var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
-    var myOptions = {
-	zoom: 4,
-	center: myLatlng,
-	mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    map = new google.maps.Map(document.getElementById("map_canvas"),
-			      myOptions);
-    
-    google.maps.event.addListener(map, 'zoom_changed', function() {
-	setTimeout(moveToDarwin, 1000);
-    });
-    
-    var marker = new google.maps.Marker({
-	position: myLatlng, 
-	map: map,
-	title:"Hello World!"
-    });
-
-    google.maps.event.addListener(marker, 'click', function() {
-	map.setZoom(8);
-    });
+  var myLatlng = new google.maps.LatLng(37.79,-122.444922);
+  var myOptions = {
+    zoom: 14,
+    center: myLatlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+  
+  google.maps.event.addListener(map, 'click', 
+				function(event) {
+				    placeMarker(event.latLng);
+				});
 }
   
-  function moveToDarwin() {
-      var darwin = new google.maps.LatLng(-12.461334, 130.841904);
-      map.setCenter(darwin);
-  }
+function placeMarker(location) {
+  var clickedLocation = new google.maps.LatLng(location);
+  var marker = new google.maps.Marker({
+      position: location, 
+      map: map
+  });
 
-
+}
