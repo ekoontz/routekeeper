82bb5930699f60d2fb20db3546d660acbee694db
Eugene Koontz <ekoontz@hiro-tan.org>
2010-08-14 09:29:29 -0700

    proxy working now.

--- server/google_proxy.js
+++ server/google_proxy.js
@@ -1,13 +1,45 @@
+var net = require('net');
 var http = require('http');
-var google = http.createClient(80, 'www.google.com');
-var request = google.request('GET', '/',
-			     {'host': 'www.google.com'});
-request.end();
-request.on('response', function (response) {
-	console.log('STATUS: ' + response.statusCode);
-	console.log('HEADERS: ' + JSON.stringify(response.headers));
-	response.setEncoding('utf8');
-	response.on('data', function (chunk) {
-		console.log('BODY: ' + chunk);
+var qs = require('querystring');
+var url = require('url');
+
+function lat(url_string) {
+    return url.parse(url_string,true).query.lat;
+}
+
+function lng(url_string) {
+    return url.parse(url_string,true).query.lng;
+}
+
+console.log("starting google proxy.");
+
+http.createServer(function (req,res) {
+
+	var google = http.createClient(80, 'maps.google.com');
+	google_url = "/maps/api/geocode/json?latlng=" + lat(req.url) + "," + lng(req.url) + "&sensor=true";
+
+	var request = google.request('GET', google_url,
+				     {'host': 'maps.google.com'});
+	request.end();
+
+	request.on('response', function (response) {
+		console.log(google_url + ' STATUS: ' + response.statusCode);
+		// content_type should normally be: 
+		//    'application/json; charset=UTF-8'
+		content_type = response.headers['content-type'];
+		console.log(" content type: " + response.headers['content-type']);
+		
+		response.setEncoding('utf8');
+		res.writeHead(200, {'Content-Type': content_type});
+
+		response.on('data', function (chunk) {
+			res.write(chunk);
+		    });
+		response.on('end',function() {
+			res.end();
+		    });
 	    });
-    });
+
+
+    }).listen(8124);
+
