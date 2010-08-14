d98696e45f8f5dc225c5cf06f56e78d6ba076bb2
Eugene Koontz <ekoontz@hiro-tan.org>
2010-08-14 07:42:06 -0700

    working on google map proxy: simple server now that listens to clients and contacts google on their behalf.

---
+++ server/google_proxy.js
@@ -0,0 +1,13 @@
+var http = require('http');
+var google = http.createClient(80, 'www.google.com');
+var request = google.request('GET', '/',
+			     {'host': 'www.google.com'});
+request.end();
+request.on('response', function (response) {
+	console.log('STATUS: ' + response.statusCode);
+	console.log('HEADERS: ' + JSON.stringify(response.headers));
+	response.setEncoding('utf8');
+	response.on('data', function (chunk) {
+		console.log('BODY: ' + chunk);
+	    });
+    });
