1e09667bc596bbe86b33e5f8e9184db1627de15e
Eugene Koontz <ekoontz@hiro-tan.org>
2010-08-14 20:44:43 -0700

    static file processing works; working on dynamic (map proxy) processing

--- server/google_proxy.js
+++ server/google_proxy.js
@@ -1,6 +1,8 @@
 var http = require('http');
 var url_req = require('url');
 var fs = require('fs');
+var fs_prefix = "/home/ekoontz/routekeeper";
+
 
 function lat(url_string) {
     console.log("url_string:" + url_string);
@@ -15,48 +17,63 @@ function pathname(url_string) {
     return url_req.parse(url_string).pathname;
 }
 
-var routes = {
-    '/routekeeper/hello.html': function (req,res) {
-	fs.readFile("/home/ekoontz/routekeeper/hello.html", function (err, data) {
-		if (err) throw err;
-		res.writeHead(200, {'Content-Type': "text/html"});
-		res.write(data);
-		res.end();
-	    });
-    },
+function routes(pathname,req,res) {
+    retval = pathname.match(new RegExp('^/[^/]+[.](js|html|css)$'));
 
-    '/map': function (req,res) {
-
-	var google = http.createClient(80, 'maps.google.com');
-	google_url = "/maps/api/geocode/json?latlng=" + lat(req.url) + "," + lng(req.url) + "&sensor=true";
-	
-	var request = google.request('GET', google_url,{'host': 'maps.google.com'});
-	request.end();
-	
-	request.on('response', function (response) {
-		console.log(google_url + ' STATUS: ' + response.statusCode);
-		// content_type should normally be: 
-		//    'application/json; charset=UTF-8'
-		content_type = response.headers['content-type'];
-		console.log(" content type: " + response.headers['content-type']);
-		
-		response.setEncoding('utf8');
-		//		res.writeHead(200, {'Content-Type': content_type});
-		// for debugging: 'text/plain'; for production: content_type.
-		res.writeHead(200, {'Content-Type': "text/plain"});
-		response.on('data', function (chunk) {
-			res.write(chunk);
-		    });
-		response.on('end',function() {
+    if (retval) {
+	var mimetype = "text/"+retval[1];
+	return function(req,res) {
+	    var filename = fs_prefix + retval[0];
+	    fs.readFile(filename, function (err, data) {
+		    if (err) {
+			console.log("500 " + req.url);
+			res.writeHead(500, {'Content-Type': "text/html"});
+			res.write("<h2>Error processing URL: " + req.url+"</h2>");
 			res.end();
-		    });
-	    });
+		    }
+		    else {
+			res.writeHead(200, {'Content-Type': mimetype});
+			res.write(data);
+			res.end();
+			console.log("200 " + req.url + "("+mimetype+")");
+		    }
+		});
+	}
     }
+    
+    if (pathname == "map") {
+	return function (req,res) {
+	    var google = http.createClient(80, 'maps.google.com');
+	    google_url = "/maps/api/geocode/json?latlng=" + lat(req.url) + "," + lng(req.url) + "&sensor=true";
+	
+	    var request = google.request('GET', google_url,{'host': 'maps.google.com'});
+	    request.end();
+	    
+	    request.on('response', function (response) {
+		    console.log(google_url + ' STATUS: ' + response.statusCode);
+		    // content_type should normally be: 
+		    //    'application/json; charset=UTF-8'
+		    content_type = response.headers['content-type'];
+		    console.log(" content type: " + response.headers['content-type']);
+		    
+		    response.setEncoding('utf8');
+		    //		res.writeHead(200, {'Content-Type': content_type});
+		    // for debugging: 'text/plain'; for production: content_type.
+		    res.writeHead(200, {'Content-Type': "text/plain"});
+		    response.on('data', function (chunk) {
+			    res.write(chunk);
+			});
+		    response.on('end',function() {
+			    res.end();
+			});
+		});
+	}
+    }
+    
 };
 
 function dispatch(pathname,req,res) {
-
-    var route = routes[pathname];
+    var route = routes(pathname,req,res);
     if (route) {
 	route(req,res);
     }
